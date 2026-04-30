2c36645d404abb701aa4c3731431f70aad028b6b
pun4drunk <pun4drunk@example.com>
2026-04-30 21:19:01 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Debug: print env vars at startup (mask values for safety)
-console.log('[DEBUG] process.env at startup:', Object.fromEntries(Object.entries(process.env).map(([k, v]) => [k, v && v.length > 0 ? '***' : v])));
 
 const config = require('./settings/config');
 const secrets = require('./settings/secrets');
